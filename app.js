b5344b99e103fe0984c0f57ba859c39e7838f292
Ishaq Afridi <ishaqafridi@example.com>
2026-05-16 00:15:01 +0500

    fresh portfolio upload

---
+++ app.js
@@ -0,0 +1,33 @@
+(function () {
+
+    [...document.querySelectorAll(".control")].forEach(button => {
+
+        button.addEventListener("click", function () {
+
+            document
+                .querySelector(".active-btn")
+                .classList.remove("active-btn");
+
+            this.classList.add("active-btn");
+
+            document
+                .querySelector(".active")
+                .classList.remove("active");
+
+            document
+                .getElementById(this.dataset.id)
+                .classList.add("active");
+
+        });
+
+    });
+
+    document
+        .querySelector(".theme-btn")
+        .addEventListener("click", () => {
+
+            document.body.classList.toggle("light-mode");
+
+        });
+
+})();
