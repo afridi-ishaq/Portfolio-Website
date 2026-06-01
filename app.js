f55f7f348b9497f98128a704e08eb06013ae9be4
Ishaq Afridi <ishaqafridi7119@gmail.com>
2026-06-01 13:29:58 +0500

    the Contact Form was enabled

--- app.js
+++ app.js
@@ -31,3 +31,36 @@
         });
 
 })();
+
+
+// EMAILJS CODE
+
+emailjs.init({
+    publicKey: "1LLHk8bPKAS9cww1O",
+});
+
+const form = document.getElementById("contact-form");
+
+form.addEventListener("submit", function (e) {
+
+    e.preventDefault();
+
+    emailjs.sendForm(
+        "service_nm031dk",
+        "template_bwktsd7",
+        this
+    )
+    .then(() => {
+
+        alert("Message Sent Successfully!");
+        form.reset();
+
+    })
+    .catch((error) => {
+
+        alert("Failed to Send Message");
+        console.log(error);
+
+    });
+
+});
